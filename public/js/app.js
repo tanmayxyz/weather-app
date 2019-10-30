d3a5ef58017be9740a2f2b8cba0ee6716c72f4d6
tanmayxyz <tanmaymaityxyz@gmail.com>
2019-10-31 00:58:03 +0530

    setup for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -19,7 +19,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     const location = search.value;
     if (location !== undefined) {
-        fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
+        fetch(`/weather?address=${location}`).then(response => {
             response.json().then(data => {
                 if (data.error) {
                     messageOne.textContent = "please provide a place"
